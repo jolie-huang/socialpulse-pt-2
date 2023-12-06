6ff748acd00d86512edadf2687e5b180810ee921
GeorgiosIoannouCoder <george_ny3@hotmail.com>
2023-12-06 17:57:39 -0500

    Create api directory

---
+++ api/extsearch.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const router = express.Router();
+const authMiddleware = require("../middleware/authMiddleware");
+const PostModel = require("../models/PostModel");
+const UserModel = require("../models/UserModel");
+
+router.get(
+  "/:username/:likesCount/:dislikesCount/:keywords",
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const { username, likesCount, dislikesCount, keywords } = req.params;
+      const searchCriteria = {};
+      if (username !== "null") {
+        const user = await UserModel.findOne({ username });
+
+        if (user) {
+          // Use the found user's _id to search for posts
+          searchCriteria["user"] = user;
+        } else {
+          // If user not found, return an empty array
+          return res.status(200).json([]);
+        }
+      }
+
+      // Get keywords
+      if (keywords !== "null") {
+        searchCriteria["keywords"] = keywords;
+      }
+
+      // Find posts based on number of likes
+      if (likesCount !== "null") {
+        searchCriteria["likesCount"] = parseInt(likesCount);
+      }
+
+      // Find posts based on number of dislikes
+      if (dislikesCount !== "null") {
+        searchCriteria["dislikesCount"] = parseInt(dislikesCount);
+      }
+      const results = await PostModel.find(searchCriteria).sort({
+        createdAt: -1,
+      });
+
+      return res.status(200).json(results);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).send("Server error!");
+    }
+  }
+);
+
+module.exports = router;
